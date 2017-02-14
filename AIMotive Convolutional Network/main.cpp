17aad4d62c4871e3ebcf94363c3c046eee95c6f8
Pilinszki-Nagy Csongor <pilinszki-nagycsongor@Pilinszki-Nagy-MacBook-Pro.local>
2017-02-14 13:04:44 +0100

    Initial Commit

---
+++ AIMotive Convolutional Network/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AIMotive Convolutional Network
+//
+//  Created by Pilinszki-Nagy Csongor on 2017. 02. 14..
+//  Copyright © 2017. Csongor Pilinszki-Nagy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
